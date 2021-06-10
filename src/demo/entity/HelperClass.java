e43d2782131fb562f5797a1ec44486e0c3446921
HoangNguyen <nguyen111@gmail.com>
2021-06-10 23:43:12 +0700

    helloJava

---
+++ src/demo/entity/HelperClass.java
@@ -0,0 +1,10 @@
+package demo.entity;
+
+public class HelperClass {
+    public double tbc(int a, int b, int c){
+        return (double)(a+b+c)/3;
+    }
+    public double chuViHinhTron( double a ){
+            return a * 3.14;
+    }
+}
